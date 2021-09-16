d45dc62bb72b2870a07a3bbfff94eb02201cf9ed
Narek <narek.babayan@optym.com>
2021-09-16 11:52:00 +0400

    socket

---
+++ backend/index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+const color = require("colors");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs").promises;
+app.use(express());
+
+const port = process.env.PORT ?? 8000;
+const logsPath = path.resolve(__dirname, "data", "logs.txt");
+
+app.use(cors());
+
+var server = app.listen(
+  port,
+  console.log(`Server is running on the port no: ${port} `.green)
+);
+
+app.get("/", async function (req, res) {
+  const data = await fs.readFile(logsPath, "utf-8");
+  const logs = data.split("\r\n").filter((i) => !!i);
+  res.json(logs);
+});
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat", (message) => {
+    io.emit("message", message);
+    message = JSON.stringify(message);
+    fs.appendFile(logsPath, `${message}\r\n`);
+  });
+  socket.on("focuses", (id, userName) => {
+    io.emit("disableInput", id, userName);
+  });
+  socket.on("blur", () => {
+    io.emit("enableInput");
+  });
+});
+
+// ========================================
+
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+// io.on("connection", (socket) => {
+//   socket.on("chat message", (msg) => {
+//     console.log("backend", msg);
+//     io.emit("chat message", msg + " world");
+//   });
+// });
+
+// server.listen(5000, () => {
+//   console.log("listening on *:5000");
+// });
+// ========================================
+// Node.js WebSocket server script
+
+// const http = require("http");
+// const express = require("express");
+// const app = express();
+// const server = http.createServer(app);
+// const WebSocket = require("ws");
+
+// const wss = new WebSocket.Server({ server });
+
+// wss.on("connection", function connection(ws) {
+//   console.log("new client connected");
+//   ws.send("welcome");
+//   ws.on("message", function incoming(message) {
+//     console.log("message", message);
+//     ws.send("message back " + message);
+//   });
+// });
+
+// app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+// server.listen(3000, () => console.log("port run:3000"));
---
+++ frontendReact/src/chat.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+
+function Chat({ socket, userName, userId }) {
+  const [message, setMessage] = useState("");
+  const [messageWhoTyping, setMessageWhoTyping] = useState("");
+  const [letters, setLetters] = useState([]);
+  const [inputDisable, setInputDisable] = useState(false);
+
+  useEffect(() => {
+    const getLettersData = async () => {
+      const response = await fetch("http://localhost:8000/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const letters = await response.json();
+      if (letters.length) {
+        setLetters(letters.map((el) => JSON.parse(el)));
+      }
+    };
+    getLettersData();
+  }, []);
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      let arrayOfLetters = letters;
+      arrayOfLetters.push({
+        userId: data.userId,
+        userName: data.userName,
+        message: data.message,
+      });
+      setLetters([...arrayOfLetters]);
+    });
+    socket.on("disableInput", (id, userName) => {
+      if (id !== userId) {
+        setInputDisable(true);
+        setMessageWhoTyping(`${userName} typing please wait ...`);
+      }
+    });
+    socket.on("enableInput", () => {
+      setInputDisable(false);
+      setMessageWhoTyping("");
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      socket.emit("blur");
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [message, inputDisable]);
+
+  const focuses = () => {
+    socket.emit("focuses", userId, userName);
+  };
+  const blur = () => {
+    socket.emit("blur");
+  };
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+    socket.emit("focuses", userId, userName);
+  };
+
+  const sendData = () => {
+    if (message !== "") {
+      socket.emit("chat", {
+        userId,
+        userName,
+        message,
+      });
+      setMessage("");
+    }
+  };
+
+  return (
+    <div className="chat">
+      <div className="user-name">
+        <h2>{userName}</h2>
+      </div>
+      <p>{messageWhoTyping}</p>
+      <input
+        type="message"
+        value={message}
+        disabled={inputDisable}
+        onChange={(e) => handleChange(e)}
+        onBlur={() => blur()}
+        onFocus={() => focuses()}
+      />
+      <div className="send">
+        <button onClick={sendData}>Send</button>
+      </div>
+      <div className="chat-message">
+        {letters.map((user, index) => {
+          if (user.userName === userName) {
+            return (
+              <div key={index} className="message">
+                <span>{user.userName}</span>
+                <p>{user.message}</p>
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} className="mess-right">
+                <span>{user.userName}</span>
+                <p>{user.message} </p>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+}
+export default Chat;
